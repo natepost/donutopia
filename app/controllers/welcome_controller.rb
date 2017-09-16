de107dea023fa63ed7f06e7926b6adfeaeca3ca6
natepost <nathanielpost@gmail.com>
2017-09-15 21:49:09 -0400

    added some basic routes and a show file to welcome views

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
   def home
   end
+
+  def show
+    @user = User.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'welcome#home'
 
+  get 'welcome/show' => '/show'
+
   end
